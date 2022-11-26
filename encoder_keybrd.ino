bf340f3ef6d226026c25e40f96753969abc162a8
sergeyhaki <navent@mail.ru>
2022-11-27 00:42:42 +0200

    Create encoder_keybrd.ino

---
+++ encoder_keybrd.ino
@@ -0,0 +1,7 @@
+//map for m64 version
+#define ENC_A PE2
+#define ENC_B PE3
+#define BTN_MENU PE6
+#define BTN_SELECT PE5
+#define BTN_OK PE4
+#define BTN_SUM ((1 << BTN_MENU) | (1 << BTN_SELECT) | (1 << BTN_OK))
