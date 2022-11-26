8de761daad58c33a1d8920c497011e38299655bb
sergeyhaki <navent@mail.ru>
2022-11-27 00:36:58 +0200

    Update lcd1602.ino

--- lcd1602.ino
+++ lcd1602.ino
@@ -1,7 +1,8 @@
 #include <LiquidCrystal.h>
-const int rs = 33, en = 32, d4 = 31, d5 = 30, d6 = 29, d7 = 28;  //m128L version
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
+LiquidCrystal lcd(33, 32, 31, 30, 29, 28);  //rs, en, d4, d5, d6, d7  m128L version
+//LiquidCrystal lcd(26, 27, 28, 29, 30, 31);  //rs, en, d4, d5, d6, d7  m64 version
+//LiquidCrystal lcd(23, 22, 21, 20, 19, 18);  //rs, en, d4, d5, d6, d7 m16 version
+ 
 void setup() {  
   lcd.begin(16, 4);
   lcd.print("hello, world!");
